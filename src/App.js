6f0da9496cd47558a0b32ed53282e2563a6e8ede
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-02 14:17:53 +0100

    export calculator to app and ensure index.js is kept clean

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+/* eslint-disable  react/prefer-stateless-function */
+import React from 'react';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
