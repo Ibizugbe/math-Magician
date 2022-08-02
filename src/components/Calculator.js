de427cdd0515719aea01374bf2a03a4140819e9f
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-02 13:07:39 +0100

    add react calculator class component

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+/* eslint-disable  react/prefer-stateless-function */
+import React from 'react';
+import 'animate.css';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Calculator;
