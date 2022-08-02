c15ec42bbb33a6713fa5c177f7a9fcceff382982
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-02 13:08:33 +0100

    add calculator structure

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,7 +5,34 @@ import 'animate.css';
 class Calculator extends React.Component {
   render() {
     return (
-      
+      <main>
+        <div className="calculator-container animate__animated animate__rubberBand">
+          <div className="calculator-display animate__animated animate__bounceInRight">
+            <h1>0</h1>
+          </div>
+          <div className="calculator-keys animate__animated animate__bounce">
+            <button type="button">AC</button>
+            <button type="button">+/-</button>
+            <button type="button">%</button>
+            <button type="button" className="bg-orange">/</button>
+            <button type="button">7</button>
+            <button type="button">8</button>
+            <button type="button">9</button>
+            <button type="button" className="bg-orange">&times;</button>
+            <button type="button">4</button>
+            <button type="button">5</button>
+            <button type="button">6</button>
+            <button type="button" className="bg-orange">-</button>
+            <button type="button">1</button>
+            <button type="button">2</button>
+            <button type="button">3</button>
+            <button type="button" className="bg-orange">+</button>
+            <button type="button" className="span-2">0</button>
+            <button type="button">.</button>
+            <button type="button" className="bg-orange">=</button>
+          </div>
+        </div>
+      </main>
     );
   }
 }
