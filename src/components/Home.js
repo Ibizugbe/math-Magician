6b3a5925f002a80f3d4a19237e4a4fbe67cbbba4
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-09 18:32:58 +0100

    add style to Project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => (
   <main>
-    <h3>Welcome to Our page</h3>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni sunt
-      nesciunt harum praesentium eius assumenda voluptatibus minus vitae ullam
-      porro excepturi vero quod at dolorem ipsam libero, debitis, quidem ut!
-    </p>
+    <div className="container-fluid p-5">
+      <h3>Welcome to Our page</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        dolorum reiciendis in distinctio architecto, natus laboriosam deleniti
+        fugit! Ab quibusdam sapiente voluptas quaerat, libero sint et quas,
+        voluptate repudiandae nobis eum vero. Similique, sapiente. Debitis nobis
+        sint eius ut. Architecto inventore ex unde non, optio recusandae nobis
+        itaque voluptates mollitia obcaecati, autem libero nesciunt veritatis
+        velit? Non, harum consectetur doloribus adipisci dolor incidunt numquam
+        voluptates! Numquam accusamus quia odio, neque unde quos repellendus
+        eligendi amet ullam facilis saepe ut sint.
+      </p>
+      <p className="pt-3">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
+        quaerat magnam dolorum, asperiores sequi velit. Dolorem dolore porro,
+        laudantium mollitia commodi, maiores voluptate dicta accusamus
+        necessitatibus dolorum laborum iure repudiandae aperiam sint quisquam
+        quo minima sit consectetur laboriosam nam? Dolorum obcaecati aliquid ex.
+        Molestiae saepe dolore magni rem error id repudiandae eligendi dicta
+        nesciunt neque assumenda, magnam, explicabo nisi maxime iste optio nemo
+        maiores minus accusamus. Odio harum distinctio ad dolorem eaque aperiam
+        adipisci modi voluptates provident rerum consequatur facilis, atque
+        quaerat hic numquam repellendus tempora. Necessitatibus iure aspernatur
+        qui.
+      </p>
+    </div>
   </main>
 );
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
@@ -11,15 +11,21 @@ export default function Nav() {
               <h3>Math Magicians</h3>
             </li>
             <li className="nav-li">
-              <ul>
-                <li className="nav-links">
-                  <Link to="/">Home</Link>
+              <ul className="nav-ul">
+                <li>
+                  <Link to="/" className="nav-links">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/components/Calculator" className="nav-links">
+                    Calculator
+                  </Link>
                 </li>
                 <li className="nav-links">
-                  <Link to="/components/Calculator">Calculator</Link>
-                </li>
-                <li className="nav-links">
-                  <Link to="/components/Quotes">Quotes</Link>
+                  <Link to="/components/Quotes" className="nav-links">
+                    Quotes
+                  </Link>
                 </li>
               </ul>
             </li>
