be669dd216f11276c2d3bb7873162f0f18e52f31
Bahoz <bahozali50@gmail.com>
2022-08-10 16:37:34 +0300

    Write some more test for testing the state user action

--- src/logic/tests/calculate.test.js
+++ src/logic/tests/calculate.test.js
@@ -18,6 +18,40 @@ describe('testing for the calculator js', () => {
     const result = calculate(state, '5');
     state = { ...state, ...result };
     expect(result.next).toBe('95');
-    console.log(state);
+  });
+
+  it('should update the state like this {total: number, next: null, operation: +} if operation and total are null', () => {
+    const result = calculate(state, '+');
+    state = { ...state, ...result };
+
+    expect(result).toEqual({ total: '95', next: null, operation: '+' });
+  });
+
+  it('should update the state like this {total: number, next: 5, operation: +} if user press number ', () => {
+    const result = calculate(state, '5');
+    state = { ...state, ...result };
+
+    expect(result).toEqual({ total: '95', next: '5', operation: '+' });
+  });
+
+  it('should update the state like this {total: number, next: null, operation: +} if state is perfect', () => {
+    const result = calculate(state, '+');
+    state = { ...state, ...result };
+
+    expect(result).toEqual({ total: '100', next: null, operation: '+' });
+  });
+
+  it('should update the state like this {total: number, next: 0., operation: +} if state is perfect', () => {
+    const result = calculate(state, '.');
+    state = { ...state, ...result };
+
+    expect(result).toEqual({ total: '100', next: '0.', operation: '+' });
+  });
+
+  it('should state be empty after user press AC', () => {
+    const result = calculate(state, 'AC');
+    state = { ...state, ...result };
+
+    expect(result).toEqual({ total: null, next: null, operation: null });
   });
 });
